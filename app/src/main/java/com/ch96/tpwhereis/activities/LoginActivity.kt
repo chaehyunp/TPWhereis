2f68ba9754a5115394b8c4486d0bf17979f02990
chhue <chhue96@gmail.com>
2023-03-29 09:54:44 +0900

    Done - Splash Activity

---
+++ app/src/main/java/com/ch96/tpwhereis/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ch96.tpwhereis.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ch96.tpwhereis.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
