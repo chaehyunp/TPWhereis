2242299d7423b557c269e0b3ce26e246dd6aabcb
chhue <chhue96@gmail.com>
2023-03-29 10:59:08 +0900

    setting - Login Activity UI Button Listener

---
+++ app/src/main/java/com/ch96/tpwhereis/activities/EmailLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ch96.tpwhereis.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ch96.tpwhereis.R
+
+class EmailLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_login)
+    }
+}
--- app/src/main/java/com/ch96/tpwhereis/activities/LoginActivity.kt
+++ app/src/main/java/com/ch96/tpwhereis/activities/LoginActivity.kt
@@ -1,12 +1,50 @@
 package com.ch96.tpwhereis.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ch96.tpwhereis.R
+import com.ch96.tpwhereis.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    val binding:ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        //둘러보기 버튼 클릭으로 로그인없이 Main화면으로 이동
+        binding.tvGo.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        //회원가입 버튼 클릭
+        binding.tvSignup.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
+
+        //이메일 로그인 버튼 클릭
+        binding.layoutEmail.setOnClickListener {
+            startActivity(Intent(this, EmailLoginActivity::class.java))
+        }
+
+        //간편 로그인 버튼 클릭
+        binding.btnLoginKakao.setOnClickListener { clickLoginKakao() }
+        binding.btnLoginGoogle.setOnClickListener{ clickLoginGoogle() }
+        binding.btnLoginNaver.setOnClickListener{ clickLoginNaver()}
+
+    }
+
+    private fun clickLoginKakao() {
+
+    }
+
+    private fun clickLoginGoogle() {
+
+    }
+
+    private fun clickLoginNaver() {
+
     }
 }
